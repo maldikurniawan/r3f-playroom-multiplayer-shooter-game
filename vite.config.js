7057870a04b8a7a659422d532bab4c647d0c5b2a
maldikurniawan <akurniawan2102@gmail.com>
2024-08-05 14:19:15 +0700

    update config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/playroom-shooter/",
+  base: "/r3f-playroom-multiplayer-shooter-game/",
   plugins: [react()],
 })
